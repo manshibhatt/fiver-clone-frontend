ef6457e6725a10a005d0fb71e99f31ac60c4d2b5
Manshi <bhattmanshi66@gmail.com>
2024-09-05 14:48:25 +0530

    commit

--- api/server.js
+++ api/server.js
@@ -27,7 +27,7 @@ const connect = async () => {
 dotenv.config(); 
  
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://fiver-clone-frontend-1fga.vercel.app", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 

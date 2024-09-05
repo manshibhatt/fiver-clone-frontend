6938579624e947cc8cafdae187097210fd5aa073
Manshi <bhattmanshi66@gmail.com>
2024-09-05 22:01:20 +0530

    commit

--- Client/src/utils/newRequest.js
+++ Client/src/utils/newRequest.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-  baseURL: "https://fiver-clone-frontend-2.onrender.com/api/",
+  baseURL: "https://fiver-clone-frontend.onrender.com/api/",
   withCredentials: true,
-});
+}); 
 
 export default newRequest;
